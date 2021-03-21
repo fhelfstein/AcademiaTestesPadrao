45d9ecf6b8196457827640cb47aa9d9a3272f711
Danilo Costa <danilo_v.barbosa@hotmail.com>
2021-03-21 02:37:08 -0300

    test(DriverManager/abstractions): modify class for implementations IDisposable interface.

--- Abstractions/DriverManager.cs
+++ Abstractions/DriverManager.cs
@@ -1,10 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using BoDi;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace AcademiaTestePadra.Abstractions
 {
-    class DriverManager
+    class DriverManager : IDisposable
     {
+        protected IWebDriver driver { get; }
+        public DriverManager()
+        {
+            driver = new ChromeDriver();
+
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+
+            string url = "http://automationpractice.com/index.php";
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public void Dispose()
+        {
+            driver?.Quit();
+            driver?.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
     }
 }
