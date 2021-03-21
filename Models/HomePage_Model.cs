55466c27cdd31bb0fcd07f6ec85b918e5252eec2
Danilo Costa <danilo_v.barbosa@hotmail.com>
2021-03-21 03:45:01 -0300

    test(HomePage_Model): add class for model homepage.

---
+++ Models/HomePage_Model.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AcademiaTestePadra.Models
+{
+    class HomePage_Model
+    {
+        private IWebDriver driver;
+        private HomePage_Page home;
+        public HomePage_Model(IWebDriver superDriver)
+        {
+            driver = superDriver;
+            home = new HomePage_Page(driver);
+        }
+
+
+
+        public void navegaCriaUsuario()
+        {
+            Assert.IsTrue(home.linkFormulario().Displayed);
+            home.linkFormulario().Click();
+            home.linkCriarUsuario().Click();
+        }
+
+        public void criaUsuario(String sNome, String sSobrenome, String sEmail)
+        {
+            Assert.IsTrue(home.txtNome().Displayed);
+            home.txtNome().SendKeys(sNome);
+            home.txtSobrenome().SendKeys(sSobrenome);
+            home.txtEmail().SendKeys(sEmail);
+            home.btnSubmit().Click();
+        }
+    }
+}
