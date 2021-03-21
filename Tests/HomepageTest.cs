a85a309a6de2137272877d60890e27ec17f97e82
Danilo Costa <danilo_v.barbosa@hotmail.com>
2021-03-21 03:02:24 -0300

    test(HomepageTest): add class for test homepage.

---
+++ Tests/HomepageTest.cs
@@ -0,0 +1,17 @@
+﻿using AcademiaTestePadra.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AcademiaTestePadra.Tests
+{
+    class HomepageTest: DriverManager
+    {
+        private HomePage_Model home;
+        public homepageTest() : base()
+        {
+            home = new HomePage_Model(this.driver);
+        }
+
+    }
+}
